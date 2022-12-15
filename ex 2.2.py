ca64cc5908a18a1e54ef2ae5628fde18910e0c70
Patrycja <p.grabowska98@gmail.com>
2022-12-15 19:25:26 +0100

    2nd exercise from python course

---
+++ ex 2.2.py
@@ -0,0 +1,16 @@
+"""
+cwiczenie na sprawdzenie działania zmiennych mutable i immutable
+
+Oto deklaracja zmiennej days:
+
+days = ['mon','tue','wed','thu','fri','sat','sun']
+należy utworzyć zmienną workdays, która początkowo będzie zawierać te same elementy co days
+następnie należy usunąć z workdays dni wolne
+na koniec wyświetl days i workdays i upewnij się, że sobota i niedziela zniknęły tylko z listy workdays
+"""
+
+days = ['mon','tue','wed','thu','fri','sat','sun']
+workdays = days.copy()
+workdays.remove('sat')
+workdays.remove('sun')
+print("days: ", days, id(days), "\nworkdays: ", workdays, id(workdays))
